9d9a0b9172a87a5bf7fc84bd29687d1784862fe2
Matias Bustos <matibustos1993@gmail.com>
2025-01-15 21:08:50 -0300

    correccion

--- entrega-final/src/public/js/products.js
+++ entrega-final/src/public/js/products.js
@@ -1,8 +1,11 @@
 const productsList = document.getElementById("products-list");
 const btnRefreshProductsList = document.getElementById("btn-refresh-products-list");
+const btnPrevPage = document.getElementById("btn-prev-page");
+const btnNextPage = document.getElementById("btn-next-page");
+let page = 1;
 
 const loadProductsList = async () => {
-    const response = await fetch("/api/products", { method: "GET" });
+    const response = await fetch(`/api/products?page=${page}`, { method: "GET" });
     const data = await response.json();
     const products = data.payload.docs ?? [];
 
@@ -17,6 +20,18 @@ const loadProductsList = async () => {
 btnRefreshProductsList.addEventListener("click", () => {
     loadProductsList();
     console.log("Lista recargada");
-})
+});
+
+btnPrevPage.addEventListener("click", () => {
+    if (page > 1) {
+        page--;
+        loadProductsList();
+    }
+});
+
+btnNextPage.addEventListener("click", () => {
+    page++
+    loadProductsList();
+});
 
 loadProductsList();
